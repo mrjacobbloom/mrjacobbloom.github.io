484b8f080616040c3125ce87a39f70a06f43c118
Jacob Bloom <mr.jacob.bloom@gmail.com>
2020-04-16 09:53:12 -0600

    working on autoplay experience for mobile

--- src/contents/resources/js/code.js
+++ src/contents/resources/js/code.js
@@ -3,10 +3,25 @@
     wrapAround: true,
     pageDots: false,
   });
+  const noop = () => 0;
+  let pausedByDefault = false;
+  carousel.on('ready', () => {
+    const firstVideo = document.querySelector(`.project-description[data-slide="0"] video`);
+    if (firstVideo.paused) {
+      firstVideo.play().catch(noop);
+      pausedByDefault = true;
+    }
+  });
   carousel.on('change', (slideId) => {
     const prevDescription = document.querySelector('.project-description.visible');
-    if (prevDescription) prevDescription.classList.remove('visible');
+    if (prevDescription) {
+      prevDescription.classList.remove('visible');
+      if (pausedByDefault) prevDescription.querySelector('video').pause().catch(noop);
+    }
     const newDescription = document.querySelector(`.project-description[data-slide="${slideId}"]`);
-    if (newDescription) newDescription.classList.add('visible');
+    if (newDescription) {
+      newDescription.classList.add('visible');
+      if (pausedByDefault) newDescription.querySelector('video').play().catch(noop);
+    }
   });
 })();
