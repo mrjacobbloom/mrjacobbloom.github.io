a6f6a88c2d77eabcd77e86d6abc02972eaace27f
Jacob Bloom <mr.jacob.bloom@gmail.com>
2020-08-14 18:57:59 -0600

    Make selected project video on carousel bigger, only selected video plays

--- resources/js/code.js
+++ resources/js/code.js
@@ -1,27 +1,38 @@
 (() => {
-  var carousel = new window.Flickity( '#projects-carousel', {
-    wrapAround: true,
-    pageDots: false,
-  });
-  const noop = () => 0;
-  let pausedByDefault = false;
-  carousel.on('ready', () => {
-    const firstVideo = document.querySelector(`.project-description[data-slide="0"] video`);
-    if (firstVideo.paused) {
-      firstVideo.play().catch(noop);
-      pausedByDefault = true;
-    }
-  });
-  carousel.on('change', (slideId) => {
+  // I don't care if the video fails to start playing, so we're swallowing those errors.
+  const noop = () => void 0;
+
+  /** @type {HTMLVideoElement | null} */ let prevVideo = null;
+
+  const onCarouselReady = () => {
+    /** @type {HTMLVideoElement} */ const firstVideo = document.querySelector(`.carousel-cell.is-selected video`);
+    firstVideo.play().catch(noop);
+    prevVideo = firstVideo;
+  };
+
+  const onCarouselChange = (slideId) => {
     const prevDescription = document.querySelector('.project-description.visible');
     if (prevDescription) {
       prevDescription.classList.remove('visible');
-      if (pausedByDefault) prevDescription.querySelector('video').pause().catch(noop);
+    }
+    if (prevVideo) {
+      prevVideo.pause();
     }
     const newDescription = document.querySelector(`.project-description[data-slide="${slideId}"]`);
     if (newDescription) {
       newDescription.classList.add('visible');
-      if (pausedByDefault) newDescription.querySelector('video').play().catch(noop);
     }
+    /** @type {HTMLVideoElement} */ const newVideo = document.querySelector(`.carousel-cell.is-selected video`);
+    newVideo.play().catch(noop);
+    prevVideo = newVideo;
+  };
+
+  new window.Flickity( '#projects-carousel', {
+    on: {
+      change: onCarouselChange,
+      ready: onCarouselReady,
+    },
+    pageDots: false,
+    wrapAround: true,
   });
 })();
